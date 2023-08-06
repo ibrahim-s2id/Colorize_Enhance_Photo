7cd3c8a67afcc233cfbbb004c39f56bf41700aa6
ibrahim-said-123 <112725611+ibrahim-said-123@users.noreply.github.com>
2023-08-06 20:56:20 +0200

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,7 +12,7 @@ export default  class Footer extends Component {
                         <p className="t">Colorize pictures with AI, turning black and white photos to color in seconds. Reimagine the past by colorizing pictures of ancestors and historic figures.</p>
                         <p className="t">Enhance and fix blurry pictures intelligently with deblur technology.</p>
                     </div>
-                    <div className="col-lg-2 col-md-4 col-12">
+                    <div className="col-lg-2 col-md-4 col-sm-4 col-12">
                         <h5 className="w">Account</h5>
                         <ul className="link">
                             <li >
@@ -29,7 +29,7 @@ export default  class Footer extends Component {
                             </li>
                         </ul>
                     </div>
-                    <div className="col-lg-2 col-md-4 col-12">
+                    <div className="col-lg-2 col-md-4 col-sm-4 col-12">
                         <h5 className="w">Navigation</h5>
                         <ul className="link">
                             <li>
@@ -49,7 +49,7 @@ export default  class Footer extends Component {
                             </li>
                         </ul>
                     </div>
-                    <div className="col-lg-3  col-md-4 col-12 social">    
+                    <div className="col-lg-3  col-md-4 col-sm-4 col-12 social">    
                         <img className="ooo" src="/images/logo.png" alt="pay-cards"/>
                     </div>
                 </div>
